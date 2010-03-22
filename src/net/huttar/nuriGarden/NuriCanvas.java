4d9823f8787219324b17f185190894940ab38f57
lars_huttar <lars_huttar@76c0ef10-b827-0410-baea-defc54adaff3>
2010-03-22 14:53:16 +0000

    Started NuriCanvas class to replace Processing applet NuriVisualizer.
    git-svn-id: svn+ssh://svn.code.sf.net/p/nurikabe-maker/code/trunk@39 76c0ef10-b827-0410-baea-defc54adaff3

---
+++ src/net/huttar/nuriGarden/NuriCanvas.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package net.huttar.nuriGarden;
+
+import javax.swing.JPanel;
+
+/**
+ * Draw board and solver state.
+ * TODO: develop this to replace NuriVisualizer.
+ */
+public class NuriCanvas extends JPanel {
+	void paintComponent() {
+		
+	}
+}
